2018c1ba1dc2de95b89df9972cf06e9a0c59e81d
SniperFox213 <dingolediego@gmail.com>
2022-08-28 15:10:55 +0300

    Initial commit

---
+++ packages/telegram-bot/src/main.rs
@@ -0,0 +1,19 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+use teloxide::prelude::*;
+
+
+#[tokio::main]
+async fn main() {
+	pretty_env_logger::init();
+	log::info!("Starting throw dice bot...");
+
+	let bot = Bot::from_env().auto_send();
+
+	teloxide::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
+		bot.send_dice(message.chat.id).await?;
+		respond(())
+	})
+	.await;
+}
